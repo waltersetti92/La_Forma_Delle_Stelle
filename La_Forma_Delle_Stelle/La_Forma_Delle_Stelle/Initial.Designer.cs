ab81445517776d32ca6419f0513f437ccfef4a9b
Walter Setti <walter.setti@iit.it>
2022-09-16 10:39:10 +0200

    Nome cambiato

--- La_Forma_Delle_Stelle/La_Forma_Delle_Stelle/Initial.Designer.cs
+++ La_Forma_Delle_Stelle/La_Forma_Delle_Stelle/Initial.Designer.cs
@@ -41,12 +41,12 @@
             this.labLuda.BackColor = System.Drawing.Color.Transparent;
             this.labLuda.Font = new System.Drawing.Font("Segoe Script", 57F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labLuda.ForeColor = System.Drawing.Color.Lime;
-            this.labLuda.Location = new System.Drawing.Point(76, 117);
+            this.labLuda.Location = new System.Drawing.Point(469, 114);
             this.labLuda.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.labLuda.Name = "labLuda";
-            this.labLuda.Size = new System.Drawing.Size(1741, 188);
+            this.labLuda.Size = new System.Drawing.Size(744, 188);
             this.labLuda.TabIndex = 4;
-            this.labLuda.Text = "LA FORMA DELLE STELLE";
+            this.labLuda.Text = "STARGATE";
             // 
             // pictureBox2
             // 
@@ -83,7 +83,7 @@
             this.Location = new System.Drawing.Point(200, 200);
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Initial";
-            this.Size = new System.Drawing.Size(1821, 831);
+            this.Size = new System.Drawing.Size(1217, 831);
             this.Load += new System.EventHandler(this.Initial_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
